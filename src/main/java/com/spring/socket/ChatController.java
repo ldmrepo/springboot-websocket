bde93f859d4001ea14aa94bad465f7ed9a23ef04
leedm <leedm68@gmail.com>
2022-11-29 23:26:35 +0900

    등록

---
+++ src/main/java/com/spring/socket/ChatController.java
@@ -0,0 +1,40 @@
+package com.spring.socket;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.simp.SimpMessageSendingOperations;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+@CrossOrigin(origins = {"http://localhost:8080"})
+@RequiredArgsConstructor
+@Controller
+public class ChatController {
+
+    private final SimpMessageSendingOperations messagingTemplate;
+
+    @MessageMapping("/chat/message")
+    public void message(ChatMessage message) {
+        if (ChatMessage.MessageType.JOIN.equals(message.getType()))
+            message.setMessage(message.getSender() + "님이 입장하셨습니다.");
+        messagingTemplate.convertAndSend("/sub/chat/room/" + message.getRoomId(), message);
+    }
+}
+
+//@RequiredArgsConstructor
+//@RestController
+//@RequestMapping("/chat")
+//public class ChatController {
+//
+//    private final ChatService chatService;
+//
+//    @PostMapping
+//    public ChatRoom createRoom(@RequestParam String name) {
+//        return chatService.createRoom(name);
+//    }
+//
+//    @GetMapping
+//    public List<ChatRoom> findAllRoom() {
+//        return chatService.findAllRoom();
+//    }
+//}
---
+++ src/main/java/com/spring/socket/ChatRoom.java
@@ -0,0 +1,44 @@
+package com.spring.socket;
+
+import lombok.Getter;
+import lombok.Setter;
+import java.util.UUID;
+
+@Getter
+@Setter
+public class ChatRoom {
+    private String roomId;
+    private String name;
+
+    public static ChatRoom create(String name) {
+        ChatRoom chatRoom = new ChatRoom();
+        chatRoom.roomId = UUID.randomUUID().toString();
+        chatRoom.name = name;
+        return chatRoom;
+    }
+}
+
+//@Getter
+//public class ChatRoom {
+//    private String roomId;
+//    private String name;
+//    private Set<WebSocketSession> sessions = new HashSet<>();
+//
+//    @Builder
+//    public ChatRoom(String roomId, String name) {
+//        this.roomId = roomId;
+//        this.name = name;
+//    }
+//
+//    public void handleActions(WebSocketSession session, ChatMessage chatMessage, ChatService chatService) {
+//        if (chatMessage.getType().equals(ChatMessage.MessageType.ENTER)) {
+//            sessions.add(session);
+//            chatMessage.setMessage(chatMessage.getSender() + "님이 입장했습니다.");
+//        }
+//        sendMessage(chatMessage, chatService);
+//    }
+//
+//    public <T> void sendMessage(T message, ChatService chatService) {
+//        sessions.parallelStream().forEach(session -> chatService.sendMessage(session, message));
+//    }
+//}
